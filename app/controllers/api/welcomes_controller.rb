477a928793305ee4deae2adb6859f9ea4d78aff5
Remington <remingtonnberg@gmail.com>
2020-10-27 16:56:46 -0500

    main

---
+++ app/controllers/api/welcomes_controller.rb
@@ -0,0 +1,11 @@
+class Api::WelcomesController < ApplicationController
+  def hello
+    @random = Random.new.rand(100)
+    render "hello.json.jb"
+  end
+
+  def about
+    @favorite = "binary"
+    render "about.json.jb"
+  end
+end
